cde6bdbec7873d995364562a59e752c00234cc66
Andrew Stepanets <andrii.stepanets@vertexinc.com>
2023-11-12 14:02:06 +0200

    Task 8

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://ev5tx39ppi.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://8ucaux8wr9.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://8ucaux8wr9.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://gweju92p0g.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
